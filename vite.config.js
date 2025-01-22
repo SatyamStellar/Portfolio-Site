f293f0d7c594d7469113e273cc974cb67a498c84
Satyam Stellar <satyam321tomar@gmail.com>
2025-01-23 03:46:07 +0530

    Update files

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['gsap'],
-      output: {
-        globals: {
-          gsap: 'gsap' // If using the external CDN
-        }
-      }
-    }
-  }
 })
